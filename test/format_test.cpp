4b3eb8aa373fb6a683dc8d2857423651e2706058
Anton Kalmykov <anton.kalmykov@gmail.com>
2016-02-01 20:32:28 +0300

    Update unit test for string formatter

---
+++ test/format_test.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <gtest/gtest.h>
+#include <tech/format.h>
+
+
+using namespace Tech;
+
+
+TEST(FormatTest, DefaultConstruction)
+{
+	String s1 = Formatter::format("This is a {0}", "test");
+	ASSERT_TRUE(s1 == "This is a test");
+
+	std::string stdString = "test";
+	String s2 = Formatter::format("This is a {0}", stdString);
+	ASSERT_TRUE(s2 == "This is a test");
+
+	std::u16string stdU16String = u"test";
+	String s3 = Formatter::format("This is a {0}", stdU16String);
+	ASSERT_TRUE(s3 == "This is a test");
+
+	ByteArray ba = "test";
+	String s4 = Formatter::format("This is a {0}", ba);
+	ASSERT_TRUE(s4 == "This is a test");
+}
