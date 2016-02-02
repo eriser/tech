00cb9e78a749015e681558d5a1bad56ad9eed851
Anton Kalmykov <anton.kalmykov@gmail.com>
2016-02-02 14:09:28 +0300

    Add stubs for WindowSystemPrivate (Windows) functions

--- src/ui/windowsystem_windows.cpp
+++ src/ui/windowsystem_windows.cpp
@@ -4,5 +4,154 @@
 namespace Tech {
 
 
+WindowSystemPrivate::WindowSystemPrivate()
+{
+
+}
+
+
+WindowSystemPrivate::~WindowSystemPrivate()
+{
+
+}
+
+
+void WindowSystemPrivate::sync()
+{
+
+}
+
+
+Widget::Handle WindowSystemPrivate::createWindow(Widget* widget, Widget::Handle owner)
+{
+	// FIXME
+	return Widget::kInvalidHandle;
+}
+
+
+void WindowSystemPrivate::destroyWindow(Widget::Handle handle)
+{
+
+}
+
+
+Widget* WindowSystemPrivate::findWindow(Widget::Handle handle) const
+{
+	// FIXME
+	return nullptr;
+}
+
+
+cairo_surface_t* WindowSystemPrivate::windowSurface(Widget::Handle handle) const
+{
+	// FIXME
+	return nullptr;
+}
+
+
+void WindowSystemPrivate::setWindowSizeLimits(Widget::Handle handle, const Size<int>& minSize,
+		const Size<int>& maxSize)
+{
+
+}
+
+
+void WindowSystemPrivate::moveWindow(Widget::Handle handle, const Point<int>& pos)
+{
+
+}
+
+
+void WindowSystemPrivate::resizeWindow(Widget::Handle handle, const Size<int>& size)
+{
+
+}
+
+
+void WindowSystemPrivate::setWindowVisible(Widget::Handle handle, bool visible)
+{
+
+}
+
+
+void WindowSystemPrivate::setWindowFrameless(Widget::Handle handle, bool enabled)
+{
+
+}
+
+
+void WindowSystemPrivate::setWindowTaskbarButton(Widget::Handle handle, bool enabled)
+{
+
+}
+
+
+void WindowSystemPrivate::setWindowTitle(Widget::Handle handle, const String& title)
+{
+
+}
+
+
+void WindowSystemPrivate::enqueueWidgetRepaint(Widget* widget)
+{
+
+}
+
+
+void WindowSystemPrivate::enqueueWidgetDeletion(Widget* widget)
+{
+
+}
+
+
+Timer::Handle WindowSystemPrivate::createTimer(Timer* timer)
+{
+	// FIXME
+	return Timer::kInvalidHandle;
+}
+
+
+void WindowSystemPrivate::destroyTimer(Timer::Handle handle)
+{
+
+}
+
+
+void WindowSystemPrivate::startTimer(Timer::Handle handle, Duration timeout, bool periodic)
+{
+
+}
+
+
+void WindowSystemPrivate::stopTimer(Timer::Handle handle)
+{
+
+}
+
+
+bool WindowSystemPrivate::isTimerActive(Timer::Handle handle) const
+{
+	return false;
+}
+
+
+Duration WindowSystemPrivate::timerInterval(Timer::Handle handle) const
+{
+	// FIXME
+	return Duration();
+}
+
+
+void WindowSystemPrivate::processEvents()
+{
+
+}
+
+
+void WindowSystemPrivate::stopEventProcessing()
+{
+
+}
+
 
 } // namespace Tech
--- src/ui/windowsystem_windows.h
+++ src/ui/windowsystem_windows.h
@@ -1,13 +1,54 @@
 #ifndef TECH_UI_WINDOWSYSTEM_WINDOWS_H
 #define TECH_UI_WINDOWSYSTEM_WINDOWS_H
 
+#include <tech/string.h>
+#include <tech/timecounter.h>
+#include <tech/ui/events.h>
+#include <tech/ui/timer.h>
+#include <tech/ui/widget.h>
+
 
 namespace Tech {
 
 
 class WindowSystemPrivate {
 public:
+	WindowSystemPrivate();
+	~WindowSystemPrivate();
 
+	void sync();
+
+	Widget::Handle createWindow(Widget* widget, Widget::Handle owner);
+	void destroyWindow(Widget::Handle handle);
+	Widget* findWindow(Widget::Handle handle) const;
+
+	cairo_surface_t* windowSurface(Widget::Handle handle) const;
+
+	void setWindowSizeLimits(Widget::Handle handle, const Size<int>& minSize,
+			const Size<int>& maxSize);
+
+	void moveWindow(Widget::Handle handle, const Point<int>& pos);
+	void resizeWindow(Widget::Handle handle, const Size<int>& size);
+
+	void setWindowVisible(Widget::Handle handle, bool visible);
+	void setWindowFrameless(Widget::Handle handle, bool enabled);
+	void setWindowTaskbarButton(Widget::Handle handle, bool enabled);
+	void setWindowTitle(Widget::Handle handle, const String& title);
+
+	void enqueueWidgetRepaint(Widget* widget);
+	void enqueueWidgetDeletion(Widget* widget);
+
+	Timer::Handle createTimer(Timer* timer);
+	void destroyTimer(Timer::Handle handle);
+
+	void startTimer(Timer::Handle handle, Duration timeout, bool periodic);
+	void stopTimer(Timer::Handle handle);
+
+	bool isTimerActive(Timer::Handle handle) const;
+	Duration timerInterval(Timer::Handle handle) const;
+
+	void processEvents();
+	void stopEventProcessing();
 };
 
 
